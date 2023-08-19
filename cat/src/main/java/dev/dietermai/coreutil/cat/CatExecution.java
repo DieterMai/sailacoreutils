baff89196b090f330dff1a8b02d52b838d81ac1a
Dieter Mai <maidieter@googlemail.com>
2023-08-19 20:57:37 +0200

    Some small refactoring

--- cat/src/main/java/dev/dietermai/coreutil/cat/CatExecution.java
+++ cat/src/main/java/dev/dietermai/coreutil/cat/CatExecution.java
@@ -63,49 +63,9 @@ class CatExecution {
 		boolean trailingRN = original.endsWith("\r\n");
 
 		
-		if(record.showNonprinting()) {
-			ByteBuffer buffer = StandardCharsets.UTF_8.encode(formatted);
-			byte[] byteArray = buffer.array();
-
-			StringBuilder sb = new StringBuilder();
-			for(byte b : byteArray) {
-				int c = Byte.toUnsignedInt(b);
-				if(c == 0) {
-					break;
-				}else if(c == '\t' || c == '\n') {
-					sb.append(c);
-				}else if(c < ' ') {
-					sb.append('^');
-					sb.append((char)(c+'@'));
-//					System.out.println("CatExecution.formatLine() "+((int)c)+" + "+((int)'@')+" = "+((char)(c+'@')));
-				}else if(c <= 126) {
-					sb.append(c);
-				}else if(c == 127) { // delete
-					sb.append("^?");
-				}else if(c <= 159){
-					sb.append("M-^");
-					sb.append((char)(c-128+'@'));
-//					System.out.println("CatExecution.formatLine() "+((int)c)+" - 128 + "+((int)'@')+" = "+((char)(c-128+'@')));
-				}else if(c < 255) {
-					sb.append("M-");
-					sb.append((char)(c-160+' '));
-//					System.out.println("CatExecution.formatLine() "+((int)c)+" - 160 + "+((int)' ')+" = "+((char)(c-160+' ')));
-				}else if(c == 255){
-					sb.append("M-^?");
-				}else{
-					sb.append("Not yet implemented");
-				}
-			}
-			formatted = sb.toString();
-		}
+		formatted = handleShowNonPrinting(formatted);
+		formatted = handleShowEnds(formatted, trailingRN, trailingN);
 		
-		// remove line ending
-		if(trailingRN) {
-			formatted = formatted.substring(0, formatted.length()-2);
-		}else if(trailingN) {
-			formatted = formatted.substring(0, formatted.length()-1);
-		}
-
 		// Add line numbers
 		if(record.numberNonblank()) {
 			if(!formatted.isEmpty() || trailingRN) {
@@ -115,24 +75,96 @@ class CatExecution {
 				formatted = "%1$ 6d  %2$s".formatted(number++, formatted);
 		}
 		
-		if(record.showEnds()) {
-			if(trailingRN) {
-				formatted = formatted + "^M$";
-			}else if(trailingN) {
-				formatted = formatted + '$';
-			}
-		}
 		
 		if(record.showTabs()) {
 			formatted = formatted.replaceAll("\\t", "^I");
 		}
-		
-		
 		
 		// remember new lines
 		pendingNewLine = trailingN;
 		return formatted;
 	}
 	
+	private String handleShowNonPrinting(String line) {
+		if(!record.showNonprinting()) {
+			return line;
+		}
+		
+		ByteBuffer buffer = StandardCharsets.UTF_8.encode(line);
+		byte[] byteArray = buffer.array();
+
+		StringBuilder sb = new StringBuilder();
+		for(byte b : byteArray) {
+			int c = Byte.toUnsignedInt(b);
+			if(c == 0) {
+				break;
+			}else if(c == '\t' || c == '\n') {
+				sb.append(c);
+			}else if(c < ' ') {
+				sb.append('^');
+				sb.append((char)(c+'@'));
+//					System.out.println("CatExecution.formatLine() "+((int)c)+" + "+((int)'@')+" = "+((char)(c+'@')));
+			}else if(c <= 126) {
+				sb.append(c);
+			}else if(c == 127) { // delete
+				sb.append("^?");
+			}else if(c <= 159){
+				sb.append("M-^");
+				sb.append((char)(c-128+'@'));
+//					System.out.println("CatExecution.formatLine() "+((int)c)+" - 128 + "+((int)'@')+" = "+((char)(c-128+'@')));
+			}else if(c < 255) {
+				sb.append("M-");
+				sb.append((char)(c-160+' '));
+//					System.out.println("CatExecution.formatLine() "+((int)c)+" - 160 + "+((int)' ')+" = "+((char)(c-160+' ')));
+			}else if(c == 255){
+				sb.append("M-^?");
+			}else{
+				sb.append("Not yet implemented");
+			}
+		}
+		return sb.toString();
+	}
+	
+	private String handleShowEnds(String line, boolean trailingRN, boolean trailingN) {
+		if(!record.showEnds()) {
+			return clipLineEnding(line);
+		}else {
+			return showEnding(line);
+		}
+	}
+	
+	private String clipLineEnding(String line) {
+		if(isCrNlEnding(line)) {
+			return line.substring(0, line.length()-2);
+		}else if(isNlEnding(line) || isCrEnding(line)) {
+			return line.substring(0, line.length()-1);
+		}else {
+			return line;
+		}
+	}
+	
+	private String showEnding(String line) {
+		if(isCrNlEnding(line)) {
+			return line.replace("\r\n", "^M$");
+		}else if(isNlEnding(line)) {
+			return line.replace('\n', '$');
+		}else if(isCrEnding(line)) {
+			return line.replace("\r", "^M");
+		}else {
+			return line;
+		}
+	}
+	
+	private boolean isCrNlEnding(String line) {
+		return line.endsWith("\r\n");
+	}
+	
+	private boolean isCrEnding(String line) {
+		return line.endsWith("\r");
+	}
+	
+	private boolean isNlEnding(String line) {
+		return line.endsWith("\n");
+	}
 	
 }
