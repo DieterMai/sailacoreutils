6ee52afaa810f8015e003fb2a70a97b35f9181f3
Dieter Mai <maidieter@googlemail.com>
2023-11-04 19:41:26 +0100

    Adapt test methods

--- cat/src/main/java/dev/dietermai/coreutil/cat/CatResult.java
+++ cat/src/main/java/dev/dietermai/coreutil/cat/CatResult.java
@@ -1,30 +1,19 @@
 package dev.dietermai.coreutil.cat;
 
-import java.util.List;
-
-public record CatResult (int exit, List<String> stdout, List<String> stderr){
+public record CatResult (int exit, String stdout, String stderr){
 	public static CatResult of(int exit) {
-		return new CatResult(exit, List.of(), List.of());
+		return new CatResult(exit, "", "");
 	}
 
-	public static CatResult of(List<String> stdout) {
-		return new CatResult(0, stdout, List.of());
-	}
-	
 	public static CatResult of(String stdout) {
-		return new CatResult(0, List.of(stdout.split("\\n")), List.of());
+		return new CatResult(0, stdout, "");
 	}
 	
-	public static CatResult ofError(int exit, List<String> stderr) {
-		return new CatResult(exit, List.of(), stderr);
+	public static CatResult ofError(int exit, String stderr) {
+		return new CatResult(exit, "", stderr);
 	}
 
-	public static CatResult of(int exit, List<String> stdout, List<String> stderr) {
+	public static CatResult of(int exit, String stdout, String stderr) {
 		return new CatResult(exit, stdout, stderr);
 	}
-	
-	public static CatResult of(int exit, String stdout, String stderr) {
-		return new CatResult(exit, List.of(stdout.split("\\n")), List.of(stderr.split("\\n")));
-	}
-
 }
--- cat/src/test/java/dev/dietermai/coreutil/cat/TestUtil.java
+++ cat/src/test/java/dev/dietermai/coreutil/cat/TestUtil.java
@@ -2,7 +2,8 @@ package dev.dietermai.coreutil.cat;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.List;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.Objects;
 
 public class TestUtil {
@@ -31,77 +32,69 @@ public class TestUtil {
 		fail(sb.toString());
 	}
 	
-	public static void descripeStringArrayDiff(String name, StringBuilder sb, List<String> expected, List<String> actual) {
+	public static void descripeStringArrayDiff(String name, StringBuilder sb, String expected, String actual) {
 		if(expected.equals(actual)) {
 			return;
 		}
+		
 		sb.append(name).append(" differs:\n");
-		sb.append("Expected lenght is ").append(expected.size()).append("\n");
-		sb.append("Actual lenght is   ").append(actual.size()).append("\n");
+		sb.append("Expected lenght is ").append(expected.length()).append("\n");
+		sb.append("Actual lenght is   ").append(actual.length()).append("\n");
 		
 		
-		for(int i = 0; i < Math.max(expected.size(), actual.size()); i++) {
-			appendStringElementDiff(sb, i, expected, actual);
-		}
+		appendStringElementDiff(sb, expected, actual);
 	}
 	
-	public static void appendStringElementDiff(StringBuilder sb, int index, List<String> expected, List<String> actual) {
-		if(index < expected.size() && index < actual.size()) {
-			return;
-		}
-		if(index >= expected.size()) {
-			sb.append("Expected: missing").append("\n");
-			sb.append("Actual: ").append(toDescriptiveString(actual.get(index))).append("\n");
-			return;
-		}
-		if(index >= actual.size()) {
-			sb.append("Expected: ").append(toDescriptiveString(expected.get(index))).append("\n");
-			sb.append("Actual: missing").append("\n");
-			return;
-		}
-		if(!Objects.equals(expected.get(index),actual.get(index))) {
-			sb.append("Expected: ").append(toDescriptiveString(actual.get(index))).append("\n");
-			sb.append("Actual: ").append(toDescriptiveString(actual.get(index))).append("\n");
-			describeFirstDiff(sb, expected.get(index), actual.get(index));
-		}
-	}
-	
-	public static String toDescriptiveString(String s) {
-		if(s == null) {
-			return "(null)";
-		}
-		if(s.isEmpty()) {
-			return s+" (empty String)";
-		}
-		if(s.isBlank()) {
-			return s+" (blank String with %s characters)".formatted(s.length());
-		}
-		return s+" (string with %s characters)".formatted(s.length());
-	}
-	
-	public static void describeFirstDiff(StringBuilder sb, String expected, String actual) {
-		for(int i = 0; i < Math.max(expected.length(), actual.length()); i++) {
-			if(i >= expected.length() && i < actual.length()) {
-				sb.append("    First diff is at char ").append(i).append(":\n");
-				sb.append("    expected has ended but actual has ").append(actual.charAt(i)).append(" [").append((int)actual.charAt(i)).append("]\n");
-				break;
-			}else if(i >= actual.length() && i < expected.length()) {
-				sb.append("    First diff is at char ").append(i).append(":\n");
-				sb.append("    actual has ended but expected has ").append(expected.charAt(i)).append(" [").append((int)expected.charAt(i)).append("]\n");
-				break;
-			}else if(expected.charAt(i) != actual.charAt(i)) {
-				sb.append("    First diff is at char ").append(i).append(":\n");
-				sb.append("    expected is ").append(expected.charAt(i)).append(" [").append((int)expected.charAt(i)).append("]\n");
-				sb.append("    actual   is ").append(actual.charAt(i)).append(" [").append((int)actual.charAt(i)).append("]\n");
-				break;
+	public static void appendStringElementDiff(StringBuilder sb, String expected, String actual) {
+		int line = 0;
+		CharacterIterator iterExp = new StringCharacterIterator(expected);
+		CharacterIterator iterAct = new StringCharacterIterator(actual);
+		
+		while(true) {
+			String expectedLine = getNextLine(iterExp);
+			String actualLine = getNextLine(iterAct);
+			if(expectedLine == null && actualLine == null) {
+				return;
 			}
+			if(expectedLine == null && actualLine != null) {
+				sb.append("Diff in line "+line+": expected line is missing but actual is '"+actualLine.translateEscapes()+"'");
+				return;
+			}
+			if(expectedLine != null && actualLine == null) {
+				sb.append("Diff in line "+line+": acutal line is missing but expected is '"+expectedLine.translateEscapes()+"'");
+				return;
+			}
+			if(!expectedLine.equals(actualLine)) {
+				sb.append("Diff in line "+line+": actual and expected differ:");
+				sb.append("\nExpected: "+expectedLine.translateEscapes());
+				sb.append("\nActual  : "+actualLine.translateEscapes());
+				return;
+			}
+			line++;
 		}
 	}
 	
-	public static String joinToLines(List<String> input) {
-		if(input.isEmpty()) {
+	private static String getNextLine(CharacterIterator iter) {
+		char c = getNext(iter);
+		if(c == CharacterIterator.DONE) {
 			return null;
 		}
-		return String.join("\n", input);
+		
+		StringBuilder sb = new StringBuilder();
+		do {
+			sb.append(c);
+			if(c == '\n') {
+				return sb.toString(); // end of line
+			}
+			c = getNext(iter);
+		}while(c != CharacterIterator.DONE);
+		
+		return sb.toString(); // end of string
+	}
+	
+	private static char getNext(CharacterIterator iter) {
+		char c = iter.current();
+		iter.setIndex(iter.getIndex()+1);
+		return c;
 	}
 }
