bba00eddcbefc3eb701487f120d5eb9131232b82
Dieter Mai <maidieter@googlemail.com>
2023-11-10 21:05:10 +0100

    Rework ConverterFactory

--- cat/src/main/java/dev/dietermai/coreutil/cat/lineconverter/ConverterFactory.java
+++ cat/src/main/java/dev/dietermai/coreutil/cat/lineconverter/ConverterFactory.java
@@ -2,6 +2,8 @@ package dev.dietermai.coreutil.cat.lineconverter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 import dev.dietermai.coreutil.cat.CatRecord;
 
@@ -9,25 +11,19 @@ public class ConverterFactory {
 	public List<ILineConverter> createConverterList(CatRecord record){
 		List<ILineConverter> converters = new ArrayList<>();
 		
-		if (record.showNonprinting()) {
-			converters.add(new ShowNonprintingConverter());
-		}
-		if (record.showTabs()) {
-			converters.add(new ShowTabsConverter());
-		}
-		if (record.showEnds()) {
-			converters.add(new ShowEndsConverter());
-		}
-		if (record.number()) {
-			converters.add(new NumberConverter());
-		}
-		if (record.numberNonblank()) {
-			converters.add(new NumberNonblankConverter());
-		}
-		if (record.squeezeBlank()) {
-			converters.add(new SqueezeBlankConverter());
-		}
+		add(converters, record::showNonprinting, ShowNonprintingConverter::new);
+		add(converters, record::showTabs, ShowTabsConverter::new);
+		add(converters, record::showEnds, ShowEndsConverter::new);
+		add(converters, record::number, NumberConverter::new);
+		add(converters, record::numberNonblank, NumberNonblankConverter::new);
+		add(converters, record::squeezeBlank, SqueezeBlankConverter::new);
 		
 		return List.copyOf(converters);
+	}
+	
+	private void add(List<ILineConverter> converters, BooleanSupplier condition, Supplier<ILineConverter> supplier) {
+		if(condition.getAsBoolean()) {
+			converters.add(supplier.get());
+		}
 	}
 }
