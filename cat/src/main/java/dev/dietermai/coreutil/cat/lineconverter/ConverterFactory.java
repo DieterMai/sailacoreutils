ea703d3b148331fe4031f9099982596000d6f710
Dieter Mai <maidieter@googlemail.com>
2023-11-10 21:12:39 +0100

    Fix --number --number-nonblank

--- cat/src/main/java/dev/dietermai/coreutil/cat/lineconverter/ConverterFactory.java
+++ cat/src/main/java/dev/dietermai/coreutil/cat/lineconverter/ConverterFactory.java
@@ -11,12 +11,12 @@ public class ConverterFactory {
 	public List<ILineConverter> createConverterList(CatRecord record){
 		List<ILineConverter> converters = new ArrayList<>();
 		
+		add(converters, record::squeezeBlank, SqueezeBlankConverter::new);
 		add(converters, record::showNonprinting, ShowNonprintingConverter::new);
 		add(converters, record::showTabs, ShowTabsConverter::new);
 		add(converters, record::numberNonblank, NumberNonblankConverter::new);
 		add(converters, record::showEnds, ShowEndsConverter::new);
-		add(converters, record::number, NumberConverter::new);
-		add(converters, record::squeezeBlank, SqueezeBlankConverter::new);
+		add(converters, () -> record.number() && !record.numberNonblank(), NumberConverter::new);
 		
 		return List.copyOf(converters);
 	}
