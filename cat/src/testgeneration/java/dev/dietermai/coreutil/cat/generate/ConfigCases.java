207057c59783f6251c77779796ce6c5fa8905d0a
Dieter Mai <maidieter@googlemail.com>
2023-11-10 20:52:30 +0100

    Correct configs

--- cat/src/testgeneration/java/dev/dietermai/coreutil/cat/generate/ConfigCases.java
+++ cat/src/testgeneration/java/dev/dietermai/coreutil/cat/generate/ConfigCases.java
@@ -31,86 +31,142 @@ public class ConfigCases {
 	}
 
 	private static ConfigCase getDefaultConfig() {
-		return new ConfigCase("default", "", "");
+		return new ConfigCase(
+				"default",
+				"",
+				"");
 	}
 
 	private static ConfigCase getShowTabsConfig() {
-		return new ConfigCase("showTabs", "--show-tabs", ".showTabs()");
+		return new ConfigCase(
+				"showTabs",
+				"--show-tabs",
+				".showTabs()");
 	}
 
 	private static ConfigCase getShowNonprintingConfig() {
-		return new ConfigCase("showNonprinting", "--show-nonprinting", ".showNonprinting()");
+		return new ConfigCase(
+				"showNonprinting",
+				"--show-nonprinting",
+				".showNonprinting()");
 	}
 
 	private static ConfigCase getShowEndsConfig() {
-		return new ConfigCase("showEnds", "--show-ends", ".showEnds()");
+		return new ConfigCase(
+				"showEnds",
+				"--show-ends",
+				".showEnds()");
 	}
 
 	private static ConfigCase getNumbersConfig() {
-		return new ConfigCase("number", "--number", ".number()");
+		return new ConfigCase(
+				"number",
+				"--number",
+				".number()");
 	}
 
 	private static ConfigCase getNumbersNoneblankConfig() {
-		return new ConfigCase("numberNoneblank", "--number-nonblank", ".numberNonblank()");
+		return new ConfigCase(
+				"numberNoneblank",
+				"--number-nonblank",
+				".numberNonblank()");
 	}
 
 	private static ConfigCase getSqueezeBlankConfig() {
-		return new ConfigCase("squeezeBlank", "--squeeze-blank", ".squeezeBlank()");
+		return new ConfigCase(
+				"squeezeBlank",
+				"--squeeze-blank",
+				".squeezeBlank()");
 	}
 
 	private static ConfigCase getShowAllConfig() {
-		return new ConfigCase("showAll", "--show-all", ".showAll()");
+		return new ConfigCase(
+				"showAll",
+				"--show-all",
+				".showAll()");
 	}
 
 	private static ConfigCase getEConfig() {
-		return new ConfigCase("e", "-e", ".e()");
+		return new ConfigCase(
+				"e",
+				"-e",
+				".e()");
 	}
 
 	private static ConfigCase getTConfig() {
-		return new ConfigCase("t", "-t", ".t()");
+		return new ConfigCase(
+				"t",
+				"-t",
+				".t()");
 	}
 
 	private static ConfigCase getUConfig() {
-		return new ConfigCase("u", "-u", ".u()");
+		return new ConfigCase(
+				"u",
+				"-u",
+				".u()");
 	}
 
 	private static ConfigCase getNumbersAndNumbersNoneblankConfig() {
-		return new ConfigCase("NumbersAndNumbersNoneblank", "--number --number-nonblank", ".number().numberNonblank()");
+		return new ConfigCase(
+				"NumbersAndNumbersNoneblank",
+				"--number --number-nonblank",
+				".number().numberNonblank()");
 	}
 
 	private static ConfigCase getNumbersAndSqueezeBlankConfig() {
-		return new ConfigCase("NumbersAndSqueezeBlank", "--number --squeeze-blank", ".number().numberNonblank()");
+		return new ConfigCase(
+				"NumbersAndSqueezeBlank",
+				"--number --squeeze-blank",
+				".number().numberNonblank()");
 	}
 
 	private static ConfigCase getNumbersNoneblankAndSqueezeBlankConfig() {
-		return new ConfigCase("NumbersNoneblankAndSqueezeBlank", " --number-nonblank --squeeze-blank",
-				".numberNonblank().numberNonblank()");
+		return new ConfigCase(
+				"NumbersNoneblankAndSqueezeBlank",
+				" --number-nonblank --squeeze-blank",
+				".numberNonblank().squeezeBlank()");
 	}
 
 	private static ConfigCase getNumbersNoneblankAndShowTabsConfig() {
-		return new ConfigCase("numbersNoneblankAndShowTabs", "--show-tabs", ".showTabs()");
+		return new ConfigCase(
+				"numbersNoneblankAndShowTabs",
+				"--show-tabs",
+				".showTabs()");
 	}
 
 	private static ConfigCase getNumbersNoneblankAndShowNonprintingConfig() {
-		return new ConfigCase("numbersNoneblankAndshowNonprinting", "--number-nonblank --show-nonprinting",
+		return new ConfigCase(
+				"numbersNoneblankAndshowNonprinting",
+				"--number-nonblank --show-nonprinting",
 				".numberNonblank().showNonprinting()");
 	}
 
 	private static ConfigCase getNumbersNoneblankAndShowEndsConfig() {
-		return new ConfigCase("numbersNoneblankAndShowEnds", "--number-nonblank --show-ends",
+		return new ConfigCase(
+				"numbersNoneblankAndShowEnds",
+				"--number-nonblank --show-ends",
 				".numberNonblank().showEnds()");
 	}
 
 	private static ConfigCase getSqueezeBlankAndShowTabsConfig() {
-		return new ConfigCase("squeezeBlankAndShowTabs", "--squeeze-blank --show-tabs", ".squeezeBlank().showTabs()");
+		return new ConfigCase(
+				"squeezeBlankAndShowTabs",
+				"--squeeze-blank --show-tabs",
+				".squeezeBlank().showTabs()");
 	}
 
 	private static ConfigCase getSqueezeBlankAndShowNonprintingConfig() {
-		return new ConfigCase("squeezeBlankAndShowNonprinting", "--squeeze-blank --show-nonprinting",
+		return new ConfigCase(
+				"squeezeBlankAndShowNonprinting",
+				"--squeeze-blank --show-nonprinting",
 				".squeezeBlank().showNonprinting()");
 	}
 
 	private static ConfigCase getSqueezeBlankAndShowEndsConfig() {
-		return new ConfigCase("squeezeBlankAndShowEnds", "--squeeze-blank --show-ends", ".squeezeBlank().showEnds()");
+		return new ConfigCase(
+				"squeezeBlankAndShowEnds",
+				"--squeeze-blank --show-ends",
+				".squeezeBlank().showEnds()");
 	}
 }
